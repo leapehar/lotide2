7fb3e26d757f7209551de2747bce3033bf3fcc4d
Lea Pehar <leapehar@gmail.com>
2022-01-17 22:14:07 +0000

    added module.export for assertEqual function. Moved test code into separate test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 // ASSERT EQUAL FUNCTION
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,8 +7,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("woohoo", "woohoo");
-assertEqual(1, 3);
+
+
+
+module.exports = assertEqual;
