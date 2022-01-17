eb54f90a87a1656c0d4b3d4a2e92d79818f73afd
Lea Pehar <leapehar@gmail.com>
2022-01-17 22:35:02 +0000

    moved test cases into seprate test file. exported tail function. imported assertEqual function.

--- tail.js
+++ tail.js
@@ -1,11 +1,7 @@
-// ASSERT EQUAL
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
-  }
-};
+// IMPORTS
+
+const assertEqual = require('./assertEqual');
+
 
 
 // TAIL FUNCTION
@@ -17,24 +13,7 @@ const tail = function(arr) {
 
 };
 
-// TAIL FUNCTION TEST CASES
-
-tail([2,5,3,7,6]);
-tail([10,20,30,40]);
-tail([4]);
-tail([]);
-
-
-// TEST CASES USING ASSERT EQUAL
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-assertEqual(words[0], "Yo Yo");
-assertEqual(words[1], "Lighthouse");
-assertEqual(words[2], "Labs");
+module.exports = tail;
 
 
 
