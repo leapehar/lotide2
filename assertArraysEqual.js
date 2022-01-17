b0e503ba215031db650fd682753e82ca430aadc2
Lea Pehar <leapehar@gmail.com>
2022-01-17 23:26:32 +0000

    exported the assertArraysEqual function and required the eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,11 @@
-// EQARRAYS FUNCTION
+// IMPORTS 
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+const eqArrays = require('../eqArrays');
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 // EQARRAYSEQUAL FUNCTION
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   let equalArrays = eqArrays(arr1, arr2);
 
   if (equalArrays) {
@@ -25,5 +15,7 @@ const assertArraysEqual = function (arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 10]);
+
+
+
+module.exports = assertArraysEqual;
