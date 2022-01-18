560b57cb88afd9f07c0b17343a48b539adb582f9
Lea Pehar <leapehar@gmail.com>
2022-01-18 00:07:34 +0000

    moved test code into separate test file. exported middle function

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-// EQARRAYS FUNCTION
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// EQARRAYSEQUAL FUNCTION
-
-const assertArraysEqual = function (arr1, arr2) {
-  let equalArrays = eqArrays(arr1, arr2);
-
-  if (equalArrays) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:${arr1} !== ${arr2}`);
-  }
-};
-
 // MIDDLE FUNCTION
 
 // Solution 1
@@ -66,45 +39,22 @@ const middle = function (arr) {
 
 // Solution 2
 
-const middle = function (arr) {
+const middle = function(arr) {
   const newArr = [];
   const middleIndex = Math.floor(arr.length / 2);
-  //console.log(middleIndex);
   if (arr.length <= 2) {
-    // console.log(newArr);
     return newArr;
   }
   if (arr.length > 2 && arr.length % 2 !== 0) {
-    //console.log(arr[middleIndex]);
     newArr.push(arr[middleIndex]);
     return newArr;
-    //console.log(newArr);
   }
   if (arr.length > 2 && arr.length % 2 === 0) {
-    //console.log(arr[middleIndex - 1], arr[middleIndex]);
     newArr.push(arr[middleIndex - 1], arr[middleIndex]);
     return newArr;
-    //console.log(newArr);
   }
-  //console.log(newArr);
-  //return newArr;
+
 };
 
-// TEST CODE
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([10, 22, 35, 42, 5000, 32, 53])); // => [42]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-// middle([1]); // => []
-// middle([1, 2]); // => []
-// middle([1, 2, 3]); // => [2]
-// middle([1, 2, 3, 4, 5]); // => [3]
-// middle([1, 2, 3, 4]); // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+module.exports = middle;
