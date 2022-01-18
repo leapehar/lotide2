95a2338d2331ade5ec141ed2d43c5ff85cbe043d
Lea Pehar <leapehar@gmail.com>
2022-01-18 03:08:42 +0000

    edited module.exports object to use Property Value Shorthand

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const assertEqual = require('./assertEqual');
-const eqArray = require('./eqArray');
+const eqArrays = require('./eqArrays');
 
 
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  eqArray: eqArray,
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
 };
