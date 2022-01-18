1c6b98897e987668beb2cae003044fb2c479a1a5
Lea Pehar <leapehar@gmail.com>
2022-01-18 02:13:52 +0000

    converted assertion tests into mocha chai assertion tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,28 +1,47 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-// const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
 
 
-// TEST CODE
+describe("#middle", () => {
+  it("returns [] for middle([1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+  it("returns [] for middle([1, 2]))", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([10, 22, 35, 42, 5000, 32, 53])); // => [42]
+  it("returns [2] for middle([1, 2, 3])", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+  it("returns [3] for middle([1, 2, 3, 4, 5])", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+  it("returns [2,3] for middle([1, 2, 3, 4])", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3])))
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 6, 7]), ([3, 4])))
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 6]), ([3])))
-console.log(assertArraysEqual(middle([1]), ([])))
+  it("returns [42] for middle([10, 22, 35, 42, 5000, 32, 53]))", () => {
+    assert.deepEqual(middle([10, 22, 35, 42, 5000, 32, 53]), [42]);
+  });
+
+  it("[42] does not equal [100]", () => {
+    assert.notDeepEqual(middle([10, 22, 35, 42, 5000, 32, 53]), [100]);
+  });
+
+  it("[42] does not equal [42,42]", () => {
+    assert.notDeepEqual(middle([10, 22, 35, 42, 5000, 32, 53]), [42, 42]);
+  });
+
+  it("returns [] which does not equal [1])", () => {
+    assert.notDeepEqual(middle([1]), [1]);
+  });
+
+  it("returns [] which does not equal [1,2])", () => {
+    assert.notDeepEqual(middle([1, 2]), [1, 2]);
+  });
+
+});
+
