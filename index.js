feee459f07b7ecefa5e99db4ef9cbce7e6ba6311
Lea Pehar <leapehar@gmail.com>
2022-01-18 02:52:37 +0000

    created one object to group together all of the functions being exported and required all of the functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqArray = require('./eqArray');
+
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  eqArray: eqArray,
+};
