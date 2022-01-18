b9863547193ec53b5bd55937006151ef5fb09b49
Lea Pehar <leapehar@gmail.com>
2022-01-18 01:03:07 +0000

    converted assertion tests to Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,20 +1,26 @@
-// IMPORTS 
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// TEST CASES FOR HEAD FUNCTION
-head([1, 2, 3]);
-head([100, 2, 3]);
-head([]);
-head([4]);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-// TEST CASES FOR ASSERT EQUAL WITH HEAD FUCNTION CALL AS AN ARGUMENT
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
 
-// PASSING
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns 'HIIIIII' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.notStrictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'HIIIIII');
+  });
 
-//FAILING
-assertEqual(head([100, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "HIIIIII");
+  it("returns '5' for ['100, 6, 7']", () => {
+    assert.notStrictEqual(head(['5']), '100');
+  });
+
+});
+
