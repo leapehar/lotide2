d57970982166415d86e8a8da5f43312a55744046
Lea Pehar <leapehar@gmail.com>
2022-01-18 01:40:37 +0000

    converted assetion tests into mocha chai assertion tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,26 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// TAIL FUNCTION TEST CASES
+describe("#tail", () => {
+  it("returns [5, 3, 7, 6] for tail([2, 5, 3, 7, 6]", () => {
+    assert.deepEqual(tail([2, 5, 3, 7, 6]), [5, 3, 7, 6]);
+  });
 
-tail([2, 5, 3, 7, 6]);
-tail([10, 20, 30, 40]);
-tail([4]);
-tail([]);
+  it("returns [20, 30, 40] for tail([10, 20, 30, 40])", () => {
+    assert.deepEqual(tail([2, 5, 3, 7, 6]), [5, 3, 7, 6]);
+  });
 
+  it("returns [20, 30, 40] for tail([10, 20, 30, 40])", () => {
+    assert.deepEqual(tail([2, 5, 3, 7, 6]), [5, 3, 7, 6]);
+  });
 
-// TEST CASES USING ASSERT EQUAL
+  it("returns [] for tail([1)", () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("[5, 3, 7, 6] is not equal to [2, 2, 2, 2]", () => {
+    assert.notDeepEqual(tail([2, 5, 3, 7, 6]), [2, 2, 2, 2]);
+  });
 
-assertEqual(words[0], "Yo Yo");
-assertEqual(words[1], "Lighthouse");
-assertEqual(words[2], "Labs");
+});
+
